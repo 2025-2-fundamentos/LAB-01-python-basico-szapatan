7a7256fe7b4f9095ea3ce0c53ac4f443fa1df1df
Simonz2 <szapatan29@gmail.com>
2025-09-17 09:44:34 -0500

    sol1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,3 +1,4 @@
+import csv
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
 datos requeridos se encuentran en el archivo data.csv. En este laboratorio
@@ -14,3 +15,12 @@ def pregunta_01():
     214
 
     """
+    sum=0
+    with open('files/input/data.csv',newline='',encoding='utf-8') as csvfile:
+        reader=csv.reader(csvfile)
+        for row in reader:
+            row1=row[0]
+            data=row1.split('\t')
+            sum+=int(data[1])
+    return sum
+
--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -1,3 +1,4 @@
+import csv
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
 datos requeridos se encuentran en el archivo data.csv. En este laboratorio
@@ -15,3 +16,17 @@ def pregunta_02():
     [('A', 8), ('B', 7), ('C', 5), ('D', 6), ('E', 14)]
 
     """
+    rta={}
+    with open('files/input/data.csv',newline='',encoding='utf-8') as csvfile:
+        reader=csv.reader(csvfile)
+        for row in reader:
+            row1=row[0].split('\t')[:2]
+            if row1[0] in rta:
+                rta[row1[0]]+=1
+            else:
+                rta[row1[0]]=1
+    data=[]
+    for k,y in rta.items():
+        data.append((k,y))
+    data.sort(key=lambda x: x[0])
+    return data
